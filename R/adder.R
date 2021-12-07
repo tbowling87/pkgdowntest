c6c1906fdb1168d050ca34ade83d700117ab1f24
Tom Bowling <tom.bowling@ascent.io>
2021-12-07 09:31:17 +0000

    add first function

---
+++ R/adder.R
@@ -0,0 +1,13 @@
+#' A function that adds
+#'
+#' @param a a number
+#' @param b another number
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' adder(2, 4)
+adder <- function(a, b) {
+  return(a + b)
+}
